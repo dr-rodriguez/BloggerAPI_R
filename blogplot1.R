d83fd2b9f91fd43b9ac2ee2aade91482648879c2
David Rodriguez <strakul@gmail.com>
2015-08-25 14:51:00 -0300

    figure creation

--- blogplot1.R
+++ blogplot1.R
@@ -8,11 +8,29 @@ library(scales)
 
 # Histogram of when I published the data
 p <- ggplot(newdata, aes(published, ..count..)) + 
-    geom_histogram() +
-    theme_bw() + xlab(NULL) +
+    geom_histogram(fill='blue', col='black') +
+    labs(x=NULL, y='Number of Posts') + 
     scale_x_datetime(breaks = date_breaks("4 months"),
                      labels = date_format("%Y-%b"),
                      limits = c(as.POSIXct("2012-01-01"), 
                                 as.POSIXct(Sys.Date())) )
 
-p
+#png('postfreq1.png', width=600)
+print(p)
+ggsave('postfreq1.png') # streches the image more nicely
+#dev.off()
+
+# Number of words through time
+#quantile(newdata$numimgs, probs=seq(0, 1, 0.25))
+quantile(newdata$numimgs, probs=c(0,0.5,0.75,0.9,0.95,0.99,1))
+# 75%-ile is 4 images
+lvls <- c(0,2,6,10,21)
+cutlvls <- cut(newdata$numimgs, lvls, include.lowest = T)
+p <- ggplot(newdata, aes(published, numwords, col=cutlvls)) +
+    geom_point(size=4) +
+    coord_cartesian() + 
+    labs(x='Date Published', y='Number of Words') + 
+    scale_color_discrete(name='Number of Images', labels=c('0-2','3-6','7-10','11-21'))
+
+print(p)
+ggsave('wordvstime1.png')
